6bc64e0efc182b2f397f89ef468e50fbda19a4e8
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-12 08:21:34 +0545

    rounded profile pic

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -59,8 +59,8 @@
                         <!-- Authentication Links -->
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
+                               data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <img src="{{ Auth::user()->profile_pic }}" alt="{{ Auth::user()->name }}" class="rounded-circle" width="30" height="100%">
                             </a>
 
                             <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
