47236994d620cd99380eda0d006ec8eeab206a9c
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-18 20:24:33 +0545

    store comment

--- app/Http/Controllers/post/CommentController.php
+++ app/Http/Controllers/post/CommentController.php
@@ -3,9 +3,27 @@
 namespace App\Http\Controllers\post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class CommentController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function store(Request $request, Post $post){
+        //validate the data
+        $this->validate($request, array(
+            'comment' => 'required|max:255'
+        ));
+        //store the data
+        $post->comments()->create([
+            'comment' => $request->comment,
+            'user_id' => $request->user()->id,
+        ]);
+
+        return redirect()->back();
+    }
 }
