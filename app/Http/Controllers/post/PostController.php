e5bc194c1120df2eee6781085adf063138ae8cec
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-18 07:23:52 +0545

    post view and save

--- app/Http/Controllers/post/PostController.php
+++ app/Http/Controllers/post/PostController.php
@@ -3,9 +3,39 @@
 namespace App\Http\Controllers\post;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Storage;
 
 class PostController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        $posts = Post::orderBy('created_at', 'desc')->get();
+        return view('home.index', compact('posts'));
+    }
+
+    public function store(Request $request)
+    {
+        //validate form data
+        $request->validate([
+            'caption' => 'required|max:500',
+            'image' => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:6048',
+        ]);
+        //save image
+        $image_path = $request->file('image')->store('uploads', 'public');
+        //store in database
+        auth()->user()->posts()->create([
+            'caption' => $request->caption,
+            'image' => $image_path,
+        ]);
+        //redirect to home page
+        return redirect()->route('home')->with('success', 'Post created successfully');
+    }
 }
