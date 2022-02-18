0bccb2447f1899191f7c1db6cdb7ea0b3f7ec762
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-18 20:09:33 +0545

    comment model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Comment extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['user_id', 'comment'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function post()
+    {
+        return $this->belongsTo(Post::class);
+    }
+
 }
