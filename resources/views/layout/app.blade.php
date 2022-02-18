c6c43e9ea1e2743edc1fe7b4ca272bd47162e32b
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-18 18:34:43 +0545

    rename modal title

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -70,7 +70,7 @@
                                 <div class="modal-dialog modal-dialog-centered modal-lg">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                            <h5 class="modal-title" id="exampleModalLabel">Add Post</h5>
                                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                                     aria-label="Close"></button>
                                         </div>
