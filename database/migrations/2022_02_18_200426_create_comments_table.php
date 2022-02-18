2da3a319752dc63460d68d39f6ee02e2f5778417
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-18 20:06:33 +0545

    comment migration

--- database/migrations/2022_02_18_200426_create_comments_table.php
+++ database/migrations/2022_02_18_200426_create_comments_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->foreignId('post_id')->constrained()->cascadeOnDelete();
+            $table->string('comment');
             $table->timestamps();
         });
     }
