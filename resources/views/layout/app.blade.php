85b8f14e879be073328f38ab74a1ee8f2cd1789b
Gaurav Pandey <pandeygaurav068@gmail.com>
2022-02-18 08:33:50 +0545

    show profile pic

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -100,7 +100,7 @@
                         <li class="nav-item dropdown">
                             <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
                                data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                                <img src="{{ Auth::user()->profile_pic }}" alt="{{ Auth::user()->name }}"
+                                <img src="/storage/{{ Auth::user()->profile_pic }}" alt="{{ Auth::user()->name }}"
                                      class="rounded-circle" width="30" height="100%">
                             </a>
 
